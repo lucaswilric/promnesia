70961f53c3bca851a7c9b697e0c318d0e4a98759
Lucas Wilson-Richter <lucas@wilric.me>
2024-11-24 22:03:14 +1100

    logseq: Link in my own graph's date format

--- src/promnesia/sources/auto_logseq.py
+++ src/promnesia/sources/auto_logseq.py
@@ -7,7 +7,14 @@ def logseq_replacer(path: str, root: str) -> str:
         return path
 
     graph = os.path.basename(root)  # noqa: PTH119
-    page_name = os.path.basename(path).rsplit('.', 1)[0]  # noqa: PTH119
+    file_name = os.path.basename(path).rsplit('.', 1)[0]  # noqa: PTH119
+    date_parts = file_name.split('_')
+    year = date_parts[0]
+    month = date_parts[1]
+    day = date_parts[2]
+
+    page_name = f"{day}-{month}-{year}"
+
     encoded_page_name = urllib.parse.quote(page_name)
 
     uri = f"logseq://graph/{graph}?page={encoded_page_name}"
